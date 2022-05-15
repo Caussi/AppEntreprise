408ba21791be71c397156be6cef2e87a9b5bacf1
Clément Caussignac <61382534+Caussi@users.noreply.github.com>
2022-05-15 23:23:17 +0200

    Add files via upload

---
+++ application_entreprise_projet.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 15 mai 2022 à 23:18
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `application_entreprise_projet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produce`
+--
+
+CREATE TABLE `produce` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `expirationDate` date DEFAULT NULL,
+  `sold` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `shoppingbag`
+--
+
+CREATE TABLE `shoppingbag` (
+  `id` int(255) NOT NULL,
+  `idUser` int(255) NOT NULL,
+  `idProduce` int(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `expirationDate` date DEFAULT NULL,
+  `noCommande` int(255) DEFAULT NULL,
+  `dateCommandeValidate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `postCode` int(255) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `trader` tinyint(1) NOT NULL,
+  `traderValidation` tinyint(1) NOT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `dateAdhesion` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `country`, `city`, `postCode`, `street`, `admin`, `trader`, `traderValidation`, `firstName`, `lastName`, `dateAdhesion`) VALUES
+(2, 'clement.coss@hotmail.fr', 'test', 'France', 'Saint-Cyr-sur-Loire', 37540, '34 Rue de la Grosse Borne', 1, 1, 1, 'ClÃ©ment', 'Caussignac', '2022-05-15');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `produce`
+--
+ALTER TABLE `produce`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `shoppingbag`
+--
+ALTER TABLE `shoppingbag`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `produce`
+--
+ALTER TABLE `produce`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `shoppingbag`
+--
+ALTER TABLE `shoppingbag`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
